2390a951dd42381b93027410dde3f3784d3c4ece
tlapata <111437411+tlapata@users.noreply.github.com>
2022-12-30 00:27:43 +0100

    Refactoring graphql.schema

---
+++ src/graphql/resolvers.ts
@@ -0,0 +1,29 @@
+import { Client } from "@apperate/iexjs";
+import GraphQLJSON from 'graphql-type-json';
+import { Resolvers } from "../generated/graphql";
+
+// IEX Finance API provider
+const client = new Client({
+    api_token: process.env.IEX_API_TOKEN, 
+    version: "v1"
+});
+const getQuote = (symbol: string) => {
+    return client.quote({symbol});    
+}
+
+// Resolver itself
+const resolvers: Resolvers = {
+    JSON: GraphQLJSON,
+
+    Query: {
+        hello: () => "That is my first Apollo Server.",
+    },
+
+    Mutation: {
+        quote: ( _: any, {symbol} ) => {
+            return getQuote(symbol);
+        }
+    }
+}
+
+export default resolvers;
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,11 @@
 require('dotenv').config();
 import { ApolloServer } from '@apollo/server';
-import { Client } from "@apperate/iexjs";
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { gql } from '@apollo/client';
-import GraphQLJSON from 'graphql-type-json';
-
-// Types definitions
-const typeDefs = gql`
-
-    scalar JSON
-
-    type Query {
-        hello: String!
-    }
-
-    type Mutation {
-        quote(symbol: String!): QuoteResult!
-    }
-
-    type QuoteResult {
-        change: Float!
-        changePercent: Float!
-        companyName: String!
-        peRatio: Float!
-        delayedPrice: Float
-        previousClose: Float!
-        symbol: String!
-    }
-`;
-
-// IEX Finance API provider
-const client = new Client({
-    api_token: process.env.IEX_API_TOKEN, 
-    version: "v1"
-});
-const getQuote = (symbol: string) => {
-    return client.quote({symbol: "AAPL"});    
-}
-
-// Resolvers
-const resolvers = {
-    JSON: GraphQLJSON,
-
-    Query: {
-        hello: () => "That is my first Apollo Server.",
-    },
-
-    Mutation: {
-        quote: ( _: any, {symbol}: {symbol: string} ) => {
-            return getQuote(symbol);
-        }
-    }
-}
+import { readFileSync } from 'fs';
+import resolvers from './graphql/resolvers';
 
 // Apollo Server
+const typeDefs = readFileSync('./src/graphql/schema.graphql', 'utf8');
 const server = new ApolloServer({
     typeDefs,
     resolvers,
